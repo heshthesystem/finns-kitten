01de550b03a09087646548fa7af918884d83f172
heshthesystem <gabriellaalevels@gmail.com>
2025-07-21 11:50:25 +0000

    decreased delay in response to "aito" keyword

--- kittenbot.py
+++ kittenbot.py
@@ -34,7 +34,7 @@ async def on_message(message):
 
     # Check for the keyword "aito" and reply with the server emoji in any channel
     if "aito" in message.content.lower():
-        await asyncio.sleep(3) 
+        await asyncio.sleep(1) 
         await message.reply("<:LionApple:1396221280168050730>")  # Replace emoji_id with the actual ID of :LionApple:
         return
 
